f18734ac4a8b1ed1e1b50168a829a818683b60dc
Code Palace <codepalace@fastmail.se>
2022-01-11 14:22:49 +0100

    First Commit

--- THREEDRotation/ContentView.swift
+++ THREEDRotation/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var degrees = 0.0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            LinearGradient(gradient: Gradient(colors: [.red.opacity(0.80), .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .cornerRadius(10)
+                .rotation3DEffect(.degrees(degrees), axis: (x: 0, y: 1, z: 0))
+            Spacer()
+            Slider(value: $degrees, in: 0...1080)
+                .padding()
+            Button("Animate") {
+                withAnimation(.easeIn(duration: 1)) {
+                    self.degrees += 180
+                }
+            }
+        }
     }
 }
 
