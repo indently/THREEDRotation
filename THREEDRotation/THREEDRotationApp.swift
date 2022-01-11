b577abfaa4c208ec42a5f790a805e66df38e8f3f
Code Palace <codepalace@fastmail.se>
2022-01-11 10:18:16 +0100

    Initial Commit

---
+++ THREEDRotation/THREEDRotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  THREEDRotationApp.swift
+//  THREEDRotation
+//
+//  Created by Federico on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct THREEDRotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
